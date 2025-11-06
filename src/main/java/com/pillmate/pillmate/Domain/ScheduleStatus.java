8c67b0f56aee2aec6975e709d2de0ce42440f3c4
KimJuHui <juhui4078@hufs.ac.kr>
2025-11-06 10:35:09 +0900

    refactor: 일정 상태 enum 정리

--- src/main/java/com/pillmate/pillmate/Domain/ScheduleStatus.java
+++ src/main/java/com/pillmate/pillmate/Domain/ScheduleStatus.java
@@ -2,7 +2,7 @@ package com.pillmate.pillmate.Domain;
 
 public enum ScheduleStatus {
     SCHEDULED,  // 예정됨
-    COMPLETED,  // 완료됨
+    TAKEN,      // 복용 완료
     MISSED,     // 미복용
     CANCELLED   // 취소됨
 }
